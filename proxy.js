3da8bd03c447ec43c95d25c69c9823a860548576
xbhuang <xbhuang1994@gmail.com>
2017-11-04 18:39:15 +0800

    support to nice hash

--- proxy.js
+++ proxy.js
@@ -1,5 +1,13 @@
-var poolListener = require('./lib/pool_listener.js');
-var minerListener = require('./lib/miner_listener.js')
+var proxyListener = require('./lib/proxy_listener');
+var simplemultialgo = require('./config.json').result.simplemultialgo;
 
-var poolSocket = poolListener.newPoolConnection(minerListener.miners);
-minerListener.createMiningListener(poolSocket);
+global.pools = new Map();
+var listeners = new Map();
+
+simplemultialgo.forEach(function(element) {
+    element.host = element.name + ".hk.nicehash.com";
+    global.pools.set(element.port,element);
+    var listener = proxyListener.createMiningListener();
+    listener.listen(element.port);
+    listeners.set(element.name,listener);
+}, this);
